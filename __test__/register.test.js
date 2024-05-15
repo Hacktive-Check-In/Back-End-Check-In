dd69bd0f5de1359b36d13eee5b8b9e5b9e68e03d
robbysaputra <robbysaputra300@gmail.com>
2024-05-15 17:06:53 +0700

    update testing

--- __test__/register.test.js
+++ __test__/register.test.js
@@ -1,9 +1,6 @@
-const request = require("supertest")
+const request = require("supertest");
 const app = require("../app");
-const { User } = require('../models')
-
-
-
+const { User } = require("../models");
 
 beforeAll(async () => {
     try {
@@ -15,45 +12,82 @@ beforeAll(async () => {
             avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
         });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-})
+});
+
 describe("POST /register", () => {
-    //* a. berhasil login
-    test(`Register Success`, async () => {
+    test("Register success", async () => {
         try {
             const response = await request(app)
-                .post(`/register`)
-                .send({ name: `admin1`, email: `admin@gmail.com`, password: `12345`, phoneNumber: `04091467`, avatar: `https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg` });
+                .post("/register")
+                .send({
+                    name: "admin2",
+                    email: "admin2@gmail.com",
+                    password: "54321",
+                    phoneNumber: "04091468",
+                    avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+                });
             const { status } = response;
-            expect(status).toBe(200);  
+            expect(status).toBe(201);
         } catch (error) {
-            console.log(error)
+            console.log(error);
         }
     });
-    test('/register failed without name', async () => {
+
+    test("/register failed without name", async () => {
         const response = await request(app)
-            .post('/register')
-            .send({ email: `admin@gmail.com`, password: `12345`, phoneNumber: `04091467`, avatar: `https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg` });
+            .post("/register")
+            .send({
+                email: "admin@gmail.com",
+                password: "12345",
+                phoneNumber: "04091467",
+                avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+            });
         const { body, status } = response;
         expect(status).toBe(400);
-        expect(body).toEqual({ message: ['name is required'] });
-    });
-    test('/register failed without email', async () => {
-        const response = await request(app)
-            .post('/register')
-            .send({ name: `admin1`, password: `12345`, phoneNumber: `04091467`, avatar: `https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg` });
-        const { body, status } = response;
-        expect(status).toBe(400);
-        expect(body).toEqual({ message: ['email is required'] });
-    });
-    test('/register failed without password', async () => {
-        const response = await request(app)
-            .post('/register')
-            .send({ name: `admin1`, phoneNumber: `04091467`, avatar: `https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg` });
-        const { body, status } = response;
-        expect(status).toBe(400);
-        expect(body).toEqual({ message: ['password is required'] });
+        expect(body).toEqual({ message: ["name is required"] });
     });
 
-})
+    test("/register failed without email", async () => {
+        const response = await request(app)
+            .post("/register")
+            .send({
+                name: "admin1",
+                password: "12345",
+                phoneNumber: "04091467",
+                avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+            });
+        const { body, status } = response;
+        expect(status).toBe(400);
+        expect(body).toEqual({ message: ["email is required"] });
+    });
+
+    test("/register failed without password", async () => {
+        const response = await request(app)
+            .post("/register")
+            .send({
+                name: "admin1",
+                email: "admin@gmail.com",
+                phoneNumber: "04091467",
+                avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+            });
+        const { body, status } = response;
+        expect(status).toBe(400);
+        expect(body).toEqual({ message: ["password is required"] });
+    });
+
+    test("/register failed without phoneNumber", async () => {
+        const response = await request(app)
+            .post("/register")
+            .send({
+                name: "admin1",
+                email: "admin@gmail.com",
+                password: "12345",
+                avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+            });
+        const { body, status } = response;
+        expect(status).toBe(400);
+        expect(body).toEqual({ message: ["phoneNumber is required"] });
+    });
+});
