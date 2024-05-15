1185d79bf06f664c3b336e10878c38f035a2617d
Nathaniel Kevin <nathaniel.kevin.untoro@gmail.com>
2024-05-15 11:38:25 +0700

    update: fixing error handler for sequelize validation and unique constraint error

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -2,7 +2,7 @@ let errorHandler = async (err, req, res, next) => {
   let status = err.status;
   let name = err.name;
   let message = err.message;
-  console.log(err, "errr");
+  console.log(err, 'errr');
   if (message === 'jwt malformed' || message === 'invalid signature') {
     res.status(401).json({ message: 'Error authentication' });
     return;
@@ -10,24 +10,20 @@ let errorHandler = async (err, req, res, next) => {
 
   switch (name) {
     case 'SequelizeValidationError':
-      let msg = err.errors.map((el) => {
-        return el.message;
-      });
+      let msg = err.errors[0].message;
       res.status(400).json({
         message: msg,
       });
       break;
     case 'SequelizeUniqueConstraintError':
-      let msg2 = err.errors.map((el) => {
-        return el.message;
-      });
+      let msg2 = err.errors[0].message;
       res.status(400).json({
         message: msg2,
       });
       break;
-    case "Invalid User":
+    case 'Invalid User':
       res.status(401).json({
-        message: "Invalid email / password",
+        message: 'Invalid email / password',
       });
       break;
     case 'missingData':
@@ -35,9 +31,9 @@ let errorHandler = async (err, req, res, next) => {
         message: 'Data not found',
       });
       break;
-    case "Invalid Input":
+    case 'Invalid Input':
       res.status(400).json({
-        message: "email / password required",
+        message: 'email / password required',
       });
       break;
     case 'validationError':
