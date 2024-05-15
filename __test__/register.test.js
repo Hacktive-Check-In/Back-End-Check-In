c4d187619550b113f29ab8b6646c8b4e948007e9
robbysaputra <robbysaputra300@gmail.com>
2024-05-15 17:19:45 +0700

    update testing registerr

--- __test__/register.test.js
+++ __test__/register.test.js
@@ -5,11 +5,10 @@ const { User } = require("../models");
 beforeAll(async () => {
     try {
         await User.create({
-            name: "admin1",
-            email: "admin@gmail.com",
-            password: "12345",
-            phoneNumber: "04091467",
-            avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+            name: "kevin",
+            password: "bebas",
+            email: "kevin@mail.com",
+            phoneNumber: "081320473251"
         });
     } catch (error) {
         console.log(error);
@@ -22,11 +21,10 @@ describe("POST /register", () => {
             const response = await request(app)
                 .post("/register")
                 .send({
-                    name: "admin2",
-                    email: "admin2@gmail.com",
-                    password: "54321",
-                    phoneNumber: "04091468",
-                    avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+                    name: "kevin",
+                    password: "bebas",
+                    email: "kevin@mail.com",
+                    phoneNumber: "081320473251"
                 });
             const { status } = response;
             expect(status).toBe(201);
@@ -39,55 +37,51 @@ describe("POST /register", () => {
         const response = await request(app)
             .post("/register")
             .send({
-                email: "admin@gmail.com",
-                password: "12345",
-                phoneNumber: "04091467",
-                avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+                password: "bebas",
+                email: "kevin@mail.com",
+                phoneNumber: "081320473251"
             });
         const { body, status } = response;
         expect(status).toBe(400);
-        expect(body).toEqual({ message: ["name is required"] });
+        expect(body).toEqual({ message: "name is required" });
     });
 
     test("/register failed without email", async () => {
         const response = await request(app)
             .post("/register")
             .send({
-                name: "admin1",
-                password: "12345",
-                phoneNumber: "04091467",
-                avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+                name: "kevin",
+                password: "bebas",
+                phoneNumber: "081320473251"
             });
         const { body, status } = response;
         expect(status).toBe(400);
-        expect(body).toEqual({ message: ["email is required"] });
+        expect(body).toEqual({ message: "email is required" });
     });
 
     test("/register failed without password", async () => {
         const response = await request(app)
             .post("/register")
             .send({
-                name: "admin1",
-                email: "admin@gmail.com",
-                phoneNumber: "04091467",
-                avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+                name: "kevin",
+                email: "kevin@mail.com",
+                phoneNumber: "081320473251"
             });
         const { body, status } = response;
         expect(status).toBe(400);
-        expect(body).toEqual({ message: ["password is required"] });
+        expect(body).toEqual({ message: "password is required"});
     });
 
     test("/register failed without phoneNumber", async () => {
         const response = await request(app)
             .post("/register")
             .send({
-                name: "admin1",
-                email: "admin@gmail.com",
-                password: "12345",
-                avatar: "https://i.pinimg.com/474x/d2/4b/be/d24bbe79387549086d159aa4462bf4c9.jpg"
+                name: "kevin",
+                password: "bebas",
+                email: "kevin@mail.com"
             });
         const { body, status } = response;
         expect(status).toBe(400);
-        expect(body).toEqual({ message: ["phoneNumber is required"] });
+        expect(body).toEqual({ message: "phone number is required" });
     });
 });
