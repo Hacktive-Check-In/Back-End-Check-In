a6e089ce4b43262b2df547989a4ecd87f6db6206
Nathaniel Kevin <nathaniel.kevin.untoro@gmail.com>
2024-05-15 12:09:34 +0700

    feat: implementing get user detail

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,54 +1,61 @@
-const { jsonwebtoken, comparePassword } = require('../helpers')
-const { User } = require('../models')
+const { jsonwebtoken, comparePassword } = require('../helpers');
+const { User } = require('../models');
 
 class UserControllers {
-    static async postRegister(req, res, next) {
-        try {
-            let { name, password, email, phoneNumber } = req.body
-            console.log(req.body, "REQ<<<<<<")
-            const data = await User.create({ name, password, email, phoneNumber })
-            console.log(data)
-            res.status(201).json({
-                message: 'Register succes',
-                id: data.id,
-                email: data.email,
-                name: data.name
-            })
-        } catch (error) {
-            next(error)
-            // console.log(error);
-        }
+  static async postRegister(req, res, next) {
+    try {
+      let { name, password, email, phoneNumber } = req.body;
+      console.log(req.body, 'REQ<<<<<<');
+      const data = await User.create({ name, password, email, phoneNumber });
+      console.log(data);
+      res.status(201).json({
+        message: 'Register succes',
+        id: data.id,
+        email: data.email,
+        name: data.name,
+      });
+    } catch (error) {
+      next(error);
+      // console.log(error);
     }
-    static async postLogin(req, res, next) {
-        try {
-            let { email, password } = req.body;
-            if (!email || !password) {
-                throw { name: "Invalid Input" }
-            }
-            const user = await User.findOne({
-                where: { email }
-            })
-            if (!user) {
-                throw { name: 'Invalid User' }
-            }
-            let compare = comparePassword(password, user.password)
-            if (!user || !compare) {
-                throw { name: 'Invalid User' }
-            }
-            let token = jsonwebtoken({
-                id: user.id
-            })
-            res.status(200).json({
-                access_token: token
-            })
-        }
-        catch (error) {
-            next(error)
-            // console.log(error);
-
-
-        }
+  }
+  static async postLogin(req, res, next) {
+    try {
+      let { email, password } = req.body;
+      if (!email || !password) {
+        throw { name: 'Invalid Input' };
+      }
+      const user = await User.findOne({
+        where: { email },
+      });
+      if (!user) {
+        throw { name: 'Invalid User' };
+      }
+      let compare = comparePassword(password, user.password);
+      if (!user || !compare) {
+        throw { name: 'Invalid User' };
+      }
+      let token = jsonwebtoken({
+        id: user.id,
+      });
+      res.status(200).json({
+        access_token: token,
+      });
+    } catch (error) {
+      next(error);
+      // console.log(error);
     }
+  }
+  static async getUserDetail(req, res, next) {
+    try {
+      const userId = req.user.id;
+      let data = await User.findAll({ where: { id: userId } });
+      res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
 }
 
-module.exports = UserControllers
+module.exports = UserControllers;
